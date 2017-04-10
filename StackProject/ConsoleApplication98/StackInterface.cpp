063ce133f065c61424e253d65064754b59187a81
miguel231997 <miguelgalindo1214@gmail.com>
2017-04-10 15:55:17 -0400

    Update StackInterface.cpp

--- StackProject/ConsoleApplication98/StackInterface.cpp
+++ StackProject/ConsoleApplication98/StackInterface.cpp
@@ -1,15 +1,4 @@
-//  Created by Frank M. Carrano and Timothy M. Henry.
-//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
 
-/** Tests whether a sequence of flights exists between two cities.
-Nonrecursive stack version.
-@pre  originCity and destinationCity both exist in the flight map.
-@post  Cities visited during the search are marked as visited
-in the flight map.
-@param  originCity The origin city.
-@param  destinationCity The destination city.
-@return  True if a sequence of flights exists from originCity
-to destinationCity; otherwise returns false. */
 bool Map::isPath(City originCity, City destinationCity)
 {
 	Stack cityStack;
